3155011853e875bb187ddf3a76509b643f76bbdb
Joy_M <233443059+Joy-Mamabolo@users.noreply.github.com>
2026-05-01 19:15:39 +0200

    refactor: Implemented proposed move function The function allows prey to propose a move based on some criteria, and the game checks if the move is valid. This will allow for more complex movement logic for the prey in the future, such as collision dynamics. A negative reward is also assigned for trying to move out of bounds for learning prey.

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 import random
 
+GRID_SIZE = 20
+NUM_PREY = 5
+
 class Agent:
     def __init__(self, x, y, speed = 1, symbol = 'A'):
         self.x = x
@@ -7,12 +10,12 @@ class Agent:
         self.speed = speed
         self.symbol = symbol
     
-    def move(self, dx, dy, grid_size):
+    def move(self, dx, dy, grid_size = GRID_SIZE):
         self.x = max(0, min(grid_size-1, self.x + dx*self.speed))
         self.y = max(0, min(grid_size-1, self.y + dy*self.speed))
 
 class Snake(Agent):
-    def __init__(self, x, y)
+    def __init__(self, x, y):
         super().__init__(x, y, speed=1, symbol = "S")
     
     def chase(self, prey_list):
@@ -32,9 +35,9 @@ class Snake(Agent):
         
         if closest_prey:
             if closest_prey.x > closest_prey.y:
-                self.move(self.speed, 0, grid_size = 20 )
+                self.move(self.speed, 0, grid_size = GRID_SIZE )
             else:
-                self.move(0, self.speed, grid_size = 20)
+                self.move(0, self.speed, grid_size = GRID_SIZE)
              
         
 class Prey(Agent):
@@ -43,11 +46,14 @@ class Prey(Agent):
         self.learning = learning
 
         self.q_table = {} if learning else None # State-action value table for Q-learning not implemented yet
-    
-    def random_move(self, grid_size):
 
-        dx, dy = random.choice([(0,1),(0,-1),(1,0),(-1,0)])
-        self.move(dx, dy, grid_size)
+        self.reward = 0 # reward received in the current step, used for learning prey. Not implemented yet
+    
+    def propose_move(self, grid_size = GRID_SIZE): # This function defines how the prey moves randomly in the environment. This will be used for the non-learning prey in the long term, but for the learning prey, this will be replaced by a Q-table based action selection function.
+
+        dx, dy = random.choice([(0,self.speed),(0,-self.speed),(self.speed,0),(-self.speed,0)])
+        
+        return dx, dy
     
     def observe(self, world):
         # This function defines how the prey observes the world 
@@ -92,18 +98,10 @@ class SafeZone:
 
         self.active = True # whether the safe zone is currently preventing snake entry. If the safe zone is beyond capacity, it becomes inactive and allows the snake to enter and capture prey inside. The safe zone becomes active again once the number of occupants falls below capacity and snake is no longer inside the safe zone
     
-    def admit(self, prey):
-        if len(self.occupants) < self.capacity:
-            self.occupants.append(prey)
-            return True
-        return False
-    
-    def release(self, prey):
-        if prey in self.occupants:
-            self.occupants.remove(prey)
+        # Deleted the list of occupants in favour of just keeping track of the number. Also don't need to track admission or release.
 
 class Game:
-    def __init__(self, grid_size = 20, num_prey = 5):
+    def __init__(self, grid_size = GRID_SIZE, num_prey = NUM_PREY):
         self.grid_size = grid_size
         self.snake = Snake(grid_size//2, grid_size//2)
         self.prey_list = [Prey(random.randint(0, grid_size-1), random.randint(0, grid_size -1)) for _ in range(num_prey)]
@@ -115,23 +113,33 @@ class Game:
                 return True
         return False
 
+    def is_in_bounds(self, x, y):
+        return False if x < 0 or x >= self.grid_size or y < 0 or y >= self.grid_size else True
+
     def step(self):
         # snake moves
-        self.snake.chase(self.prey_list)
+        self.snake.chase(self.prey_list) # Snake needs to be prevented from moving into the safe zone if it is active.
 
         #prey moves
         for prey in self.prey_list:
-            prey.random_move(self.grid_size)
+            dx, dy = prey.propose_move()
+
+            if self.is_in_bounds(prey.x + dx, prey.y + dy):
+                prey.move(dx, dy, grid_size = self.grid_size)
+            else:
+                prey.move(0, 0, grid_size = self.grid_size) # if proposed move is out of bounds, stay in place
+                prey.reward = -1 # negative reward for trying to move out of bounds. This is for learning prey.
         
         # check safe zone admissions
-        for safe_zone in self.safe_zone: # there is only one safe zone for now but this allows for more than one should we wish
+        for sz in self.safe_zone: # there is only one safe zone for now but this allows for more than one should we wish
             for prey in self.prey_list:
-                if (safe_zone.x<=prey.x<=safe_zone.x+safe_zone.size) and (safe_zone.y<=prey.y<=safe_zone.y+safe_zone.size):
-                    admit = safe_zone.admit(prey)
+                if (sz.x<=prey.x<=sz.x+sz.size) and (sz.y<=prey.y<=sz.y+sz.size):
+                    sz.current_occupants +=1
 
-                    if not admit:
+                    if sz.active and sz.current_occupants >= sz.capacity:
+                        sz.active = False
+                    else:
 
-                        safe_zone.release()
 
 
 
